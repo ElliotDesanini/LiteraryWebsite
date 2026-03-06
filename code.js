5034706c3211bdc80a7981e6d4d04916ef8ce77d
SludgeCode <elliot.desanini@icloud.com>
2026-03-06 10:23:53 +0100

    edit

--- code.js
+++ code.js
@@ -1,8 +1,9 @@
-//import textData from "/text.json";
-
 let file = await fetch("/text.json")
 let content = await file.json()
-console.log(content)
+
 let language = "english"
 
-document.getElementById("ravenPoem").innerHTML = content[language]["ravenPoem"].join("\n")
+const idArray = ["ravenPoem"]
+
+
+document.getElementById("ravenPoem").innerHTML = content[language]["ravenPoem"].join("")
