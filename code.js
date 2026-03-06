8e5cc07fbbf7355ac308339a54aa9003b4c89738
SludgeCode <elliot.desanini@icloud.com>
2026-03-06 11:45:49 +0100

    building hero

--- code.js
+++
@@ -1,9 +0,0 @@
-let file = await fetch("/text.json")
-let content = await file.json()
-
-let language = "english"
-
-const idArray = ["ravenPoem"]
-
-
-document.getElementById("ravenPoem").innerHTML = content[language]["ravenPoem"].join("")
